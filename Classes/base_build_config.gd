998bc36a7b2e71336d4a3a57614bdb6bf009d9cd
carllacan <carles.llacer@gmail.com>
2025-12-19 08:23:40 +0100

    Add more features to BaseBuildConfig

--- Classes/base_build_config.gd
+++ Classes/base_build_config.gd
@@ -8,7 +8,16 @@ enum ForceActions
 	ForceFalse,
 }
 
+@export_category("Launch settings")
+@export var use_testing_savefile:bool = false
+@export var testing_savefile:GameState
+@export var skip_main_menu:bool = false
+@export var force_new_game:bool = false
 
+@export_category("Integration testing")
+@export var clear_achievements_at_start:bool = false
+
+@export_category("Flag management")
 @export var force_debug:ForceActions = ForceActions.None
 @export var force_demo:ForceActions = ForceActions.None
 @export var force_web:ForceActions = ForceActions.None
@@ -16,7 +25,9 @@ enum ForceActions
 @export var force_itchio:ForceActions = ForceActions.None
 
 
-# Forces a bool to a value depending on a force value, and returns it
+# Forces a bool to a value depending on a force value, and returns either the 
+# original or the forced value. To be use mainly in the global Flags, so they
+# can be overridden.
 # Ex: BuildConfig.Default.force_flag(OS.has_feature("demo"), ForceActions.ForceTrue)
 func force_flag(original_value:bool, force_value:ForceActions)-> bool:
 	match force_value:
@@ -29,3 +40,15 @@ func force_flag(original_value:bool, force_value:ForceActions)-> bool:
 			
 	push_error("Unexpected enum value")
 	return false
+
+
+
+static func get_default_build()-> BaseBuildConfig:
+	var b:BaseBuildConfig
+	if OS.has_feature("editor"):
+		b = load("res://Parameters/editor_build_config.tres")
+	else:
+		b = load("res://Parameters/release_build_config.tres")
+	if b == null:
+		push_error("No build configuration found")
+	return b
