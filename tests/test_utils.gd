79d1ac47c87336f5d9f46cb241ba8d4b68426fa6
carllacan <carles.llacer@gmail.com>
2026-01-09 18:35:45 +0100

    Add GUT tests for the Utils class

--- tests/test_utils.gd
+++ tests/test_utils.gd
@@ -1,5 +1,6 @@
 extends GutTest
 
+
 func test_single_segment_no_threshold_crossed():
 	var thresholds:Array[float] = [10.0]
 	var slopes:Array[float] = [2.0, 5.0]
@@ -7,6 +8,7 @@ func test_single_segment_no_threshold_crossed():
 	var result := Utils.piecewise_linear(5.0, thresholds, slopes)
 
 	assert_eq(result, 2.0 * 5.0)
+
 
 func test_second_segment_after_threshold():
 	var thresholds:Array[float] = [10.0]
@@ -18,6 +20,7 @@ func test_second_segment_after_threshold():
 	# Result: 20 + 5*5 = 45
 	assert_eq(result, 45.0)
 
+
 func test_multiple_thresholds_middle_segment():
 	var thresholds:Array[float] = [10.0, 20.0]
 	var slopes:Array[float] = [1.0, 2.0, 3.0]
@@ -28,6 +31,7 @@ func test_multiple_thresholds_middle_segment():
 	# Result: 10 + 5*2 = 20
 	assert_eq(result, 20.0)
 
+
 func test_continuity_at_threshold():
 	var thresholds:Array[float] = [10.0]
 	var slopes:Array[float] = [2.0, 5.0]
@@ -37,6 +41,7 @@ func test_continuity_at_threshold():
 
 	assert_almost_eq(left, right, 0.01)
 
+
 func test_exactly_at_threshold_uses_previous_piece():
 	var thresholds:Array[float] = [10.0]
 	var slopes:Array[float] = [2.0, 5.0]
@@ -44,6 +49,7 @@ func test_exactly_at_threshold_uses_previous_piece():
 	var result := Utils.piecewise_linear(10.0, thresholds, slopes)
 
 	assert_eq(result, 20.0)
+
 
 func test_negative_x_uses_first_slope():
 	var thresholds:Array[float] = [10.0]
