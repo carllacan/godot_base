980de91ba7e24f1809fb26d5afdccb92e138a9fb
carllacan <carles.llacer@gmail.com>
2026-01-20 18:53:33 +0100

    Fixed random entry generation

--- Sequences/sequencer.gd
+++ Sequences/sequencer.gd
@@ -1,8 +1,15 @@
 extends Node
 class_name Sequencer
 
+enum EndMode {
+	STOP,
+	CYCLE_BACK,
+	MAINTAIN_LAST,
+}
 
 var sequence:Sequence
+var end_mode:EndMode = EndMode.MAINTAIN_LAST
+
 var elements:Array[Variant] = []
 var last_yield_idx:int = 0
 
@@ -19,18 +26,29 @@ func restart()-> void:
 		
 	elements = []
 	for entry in sequence.entries:
-		if entry is RandomSequenceEntry:
-			entry = Utils.rand_weighted(entry.entries)
-		for i in range(entry.repeats):
-			elements.append(entry.content)
+		if entry is RandomSequenceEntry:			
+			for i in range(entry.repeats):
+				var random_entry = Utils.rand_weighted(entry.entries)
+				elements.append(random_entry.content)
+		else:
+			for i in range(entry.repeats):
+				elements.append(entry.content)
 		
 	last_yield_idx = 0
-
-
+	
+			
 func get_next()-> Variant:
+	if last_yield_idx >= len(elements):
+		match end_mode:
+			EndMode.STOP:
+				return null
+			EndMode.CYCLE_BACK:
+				last_yield_idx -= len(elements)
+			EndMode.MAINTAIN_LAST:
+				return elements[-1]
+							
 	var next = elements[last_yield_idx]
-	last_yield_idx += 1
-	if last_yield_idx == len(elements):
-		last_yield_idx -= len(elements)
+	
+	last_yield_idx += 1	
 		
 	return next
