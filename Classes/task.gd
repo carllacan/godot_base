8fdb2674f2e0f3b0a61cfa06990139ae1660541f
carllacan <carles.llacer@gmail.com>
2026-02-26 22:13:43 +0100

    Added method to quickly make a tasks that checks something on a signal

--- Classes/task.gd
+++ Classes/task.gd
@@ -5,8 +5,15 @@ signal done
 
 var result
 
-func set_result(new_value)-> void:
+func set_result(new_value=null)-> void:
 	result = new_value
 	done.emit(result)
 
 	
+static func check_on_signal(target_signal:Signal, check:Callable)-> Task:	
+	var t = Task.new()
+	target_signal.connect(func(): 
+		if check.call():
+			t.set_result()
+		)
+	return t
