230aefd4eb7b97670ca5e8f459622bfde5dbccac
carllacan <carles.llacer@gmail.com>
2026-02-26 22:12:47 +0100

    Implemented GameResource management in base GameState class

--- Classes/base_game_run.gd
+++ Classes/base_game_run.gd
@@ -1,11 +1,19 @@
 extends Resource
 class_name BaseGameState
 
-
 const DEFAULT_FILENAME = "save.tres"
 const INITIAL_RUN_FILEPATH = "res://Data/GameRuns/initial_game_run.tres"
 const DEMO_INITIAL_RUN_FILEPATH = "res://Data/GameRuns/demo_initial_game_run.tres"
 
+
+signal resources_changed
+signal resource_changed(resource:GameResource, new_value:float)
+signal resource_revealed(resource:GameResource)
+
+@export_group("Resources")
+@export var current_resources:Dictionary[GameResource, float] = {}
+@export var total_collected_resources:Dictionary[GameResource, float] = {}
+@export var revealed_resources:Array[GameResource] = []
 @export_group("Others")
 @export var timestamp_unix:float = 0
 @export var version:String
@@ -71,3 +79,87 @@ static func load(filepath:String)-> BaseGameState:
 	
 	
 #endregion Saving
+
+#region Resource managaement
+
+#region Resources
+	
+func get_current_resource(res:GameResource)-> float:
+	return current_resources.get(res, 0)
+	
+	
+func is_resource_revealed(res:GameResource)-> bool:
+	if res in revealed_resources:
+		return true
+	else:
+		return false
+		
+		
+func reveal_resource(resource:GameResource)-> void:
+	if not resource in current_resources.keys():
+		set_resource(resource, 0)
+	revealed_resources.append(resource)
+	resource_revealed.emit(resource)
+	
+	
+func set_resource(resource:GameResource, value:float)-> void:
+	var old_value:float = current_resources.get(resource, 0.0)
+	current_resources[resource] = value
+
+	if value > 0 and resource not in revealed_resources:
+		reveal_resource(resource)
+
+	if old_value != value:
+		resource_changed.emit(resource, value)
+		SignalManager.emit_this_frame(changed)
+		SignalManager.emit_this_frame(resources_changed)
+
+
+func increase_resources(amount:Dictionary[GameResource, float])-> void:
+	for c in amount.keys():
+		current_resources[c] += amount[c]
+
+		if current_resources[c] > 0 and c not in revealed_resources:
+			revealed_resources.append(c)
+			resource_revealed.emit(c)
+
+		if c not in total_collected_resources.keys():
+			total_collected_resources[c] = 0
+		total_collected_resources[c] += amount[c]
+
+		resource_changed.emit(c, current_resources[c])
+
+	SignalManager.emit_this_frame(changed)
+	SignalManager.emit_this_frame(resources_changed)
+
+
+func decrease_resources(amount:Dictionary[GameResource, float])-> void:
+	if BuildConfig.Default.blank_check:
+		push_warning("Omitting decrease_resources because blank_check is active")
+		return
+
+	for c in amount.keys():
+		if is_nan(amount[c]): continue
+		assert(current_resources[c] >= amount[c],
+		"Can't take %s %s from %s" % [amount[c], c.dname, current_resources[c]])
+		current_resources[c] -= amount[c]
+		resource_changed.emit(c, current_resources[c])
+
+	SignalManager.emit_this_frame(changed)
+	SignalManager.emit_this_frame(resources_changed)
+	
+	
+# this needs to be typed with GameResource, otherwise it conflicts when you
+# use prices typed with GameResource
+func can_afford(price:Dictionary[GameResource, float])-> bool:
+	if BuildConfig.Default.blank_check:
+		push_warning("Omitting can_afford checks because blank_check is active")
+		return true
+
+	for c in price.keys():
+		if current_resources[c] < price[c]:
+			return false
+
+	return true
+	
+#endregion
