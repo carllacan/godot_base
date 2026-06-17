8d0d60863d460cec7134bda2862213c625a35da0
carllacan <carles.llacer@gmail.com>
2026-06-17 12:45:28 +0200

    Don't set the autoplayer if the one in the BuildConfig is null

--- Autoplayers/autoplayer_manager.gd
+++ Autoplayers/autoplayer_manager.gd
@@ -5,7 +5,8 @@ var player:BaseAutoplayer
 
 
 func _ready()-> void:
-	player = BuildConfig.Default.autoplayer
+	if BuildConfig.Default.autoplayer != null:
+		player = BuildConfig.Default.autoplayer
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
