7cb54c1f9dadd08553832910f9a51cc7b4391161
carllacan <carllacan@gmail.com>
2026-09-12 12:28:42 +0200

    ChangeTrigger improvements

--- Classes/change_trigger.gd
+++ Classes/change_trigger.gd
@@ -3,23 +3,29 @@ class_name ChangeTrigger
 
 ## Todo: move out of Components folder
 
-signal integer_part_changed
+signal change_detected
 
 ## Emits a signals when a quantity crosses certain thresholds. For instance,
-## every time it changes by at least 1 unit.
+## if min_value = 1 and max_value = INF then the signal will trigger every
+## time the amount changes by 1
 
 ## Minimum value to trigger the signal
-@export var min_value:float = NAN
+@export var min_value:float = -INF
 ## Maximum value to trigger the signal
-@export var max_value:float = NAN
+@export var max_value:float = INF
+## Value against which the quantity will be compared the first time. Leave
+## to the default NAN value to skip the first comparison.
+@export var initial_value:float = NAN
+
 
 var old_value:float = NAN
 
 
-
 func update_amount(new_value:float)-> void:
+	if is_nan(old_value):
+		old_value = initial_value
 	
-	if not is_nan(old_value) and not is_nan(new_value):
+	if not is_nan(new_value):
 		react_to_changes(old_value, new_value)
 			
 	# Update stored amount
@@ -36,4 +42,4 @@ func react_to_changes(prev_value:float, new_value:float)-> void:
 		
 	# Detect changes in the integer part
 	if int(prev_value) != int(new_value):
-		integer_part_changed.emit()
+		change_detected.emit()
